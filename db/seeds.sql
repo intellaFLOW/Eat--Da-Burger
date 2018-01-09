4c51c5a5e526481d72fffa97f6b89f211f3adb03
intellaFLOW <tparmar1988@gmail.com>
2018-01-09 15:18:29 -0800

    working app

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Garden Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Kobe Beef Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', true);
